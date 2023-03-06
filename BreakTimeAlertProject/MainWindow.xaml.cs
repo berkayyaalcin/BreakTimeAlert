a8a1eaced04e6a8bf50c194aac2995f5ebd6c074
berkayyaalcin <berkayyaalcin@gmail.com>
2023-03-06 13:54:48 +0300

    #1_MainWindow-Sablon-Ayarlandi

--- BreakTimeAlertProject/MainWindow.xaml.cs
+++ BreakTimeAlertProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,15 @@ namespace BreakTimeAlertProject
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Pencerenin kapatılma özelliği iptal edilerek gizlenmesi olarak değiştirildi.
+        /// </summary>
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
         }
     }
 }
