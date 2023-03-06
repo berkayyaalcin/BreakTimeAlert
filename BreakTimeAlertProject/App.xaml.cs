aa9da45b76726a0391c41a867c46482257185803
berkayyaalcin <berkayyaalcin@gmail.com>
2023-03-06 20:43:11 +0300

    #4_Coklu-Ekran-Yansıtması

--- BreakTimeAlertProject/App.xaml.cs
+++ BreakTimeAlertProject/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BreakTimeAlertProject.Properties;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -18,6 +19,35 @@ namespace BreakTimeAlertProject
         {
             base.OnStartup(e);
             tray.AppIcon();
+            var timer = new System.Timers.Timer(TimeSpan.FromMinutes(20).TotalMilliseconds);
+
+            //Tüm ekranları gezerek screen'e atayarak her bir ekran için yeni bir Mainwindow nesnesi oluşturup konumu boyutu gibi özellikleri verip gizler.
+            foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.WindowStartupLocation = WindowStartupLocation.Manual;
+                mainWindow.Left = screen.WorkingArea.Left;
+                mainWindow.Top = screen.WorkingArea.Top;
+                mainWindow.Hide();
+                mainWindow.WindowState = WindowState.Normal;
+
+                //Belirtilen süre aralıklarla çalışması üzerine ayarlanmış ve timer çağrısı ile tetiklenecektir.
+                timer.Elapsed +=
+                        delegate (object? sender, System.Timers.ElapsedEventArgs args)
+                        {
+                            //Pencereler ekranda gösterilir ve odaklanıp etkinleştirilir. Invoke belirtilen işlevi UI üzerinde çalıştırmak için kullanılır.
+                            mainWindow.Dispatcher.Invoke(() =>
+                            {
+                                mainWindow.Show();
+                                mainWindow.WindowState = WindowState.Maximized;
+                                mainWindow.Activate();
+                                mainWindow.Focus();
+                            });
+                            timer.Stop();
+                            timer.Start();
+                        };
+                timer.Start();
+            }
         }
     }
 }
